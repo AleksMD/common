47ed3fb5221d3c8829f805667fbd04c74d5a9d05
AleksMD <budjonniy291@gmail.com>
2019-10-06 22:49:44 +0300

    Second commit. Improve code style. Modify function #3

--- tests_practice/my_func.py
+++ tests_practice/my_func.py
@@ -2,28 +2,18 @@ import math
 import string
 
 
-
-a =[1,1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-some_str = 'I am a good developer. I am also a writer'
-int_list = [0,2,0,4,0,7,10]
-l = [5, 3, 4, 3, 4]
-l1 = [1,2,3,4,6,7,8]
-
 def func_1(a, b):
     return list(set(a) & set(b))
 
 
-def func_2(string):
-    return string.count('a')
+def func_2(any_str):
+    return any_str.count('a')
 
-def func_3(num, _pow=3):
-    flag = False
-    for i in range(num):
-        if i ** _pow == num:
-            flag = True
-            break
-    return flag
+
+def func_3(num):
+    x = math.log(num, 3)
+    return int(x) == x
+
 
 def func_4(num):
     if len(str(num)) > 1:
@@ -35,9 +25,11 @@ def func_4(num):
     else:
         return num
 
-def func_5(sqc: list):
-    sqc.sort(key=lambda i: i == 0)
-    return sqc
+
+def func_5(sqnc: list):
+    sqnc.sort(key=lambda i: i == 0)
+    return sqnc
+
 
 def func_6(sqnc: list):
     start = sqnc[0]
@@ -45,14 +37,17 @@ def func_6(sqnc: list):
     stop = sqnc[-1]
     return list(range(start, stop+1, step)) == sqnc
 
+
 def func_7(l):
     for i in l:
         if l.count(i) == 1:
             return i
 
+
 def func_8(l):
     contr_list = list(range(l[0], l[-1]+1))
     return list(set(contr_list) ^ set(l))[0]
+
 
 def func_9(l):
     count = 0
@@ -63,18 +58,23 @@ def func_9(l):
             count += 1
     return count
 
+
 def func_10(s):
     return s[::-1]
 
+
 def func_11(num):
     return f'{num // 60}:{num % 60}'
+
 
 def func_12(s):
     s = "".join((char if char.isalpha() else " ") for char in s).split()
     return max(s, key=len)
 
+
 def func_13(s):
     return ' '.join(s.split()[::-1])
+
 
 def func_14(num):
     a = 0
@@ -88,8 +88,10 @@ def func_14(num):
 
     return fib_list
 
+
 def func_15(n: list):
     return [i for i in n if i % 2 == 0]
+
 
 def func_16(rang: int):
     sum_of_range = 0
@@ -97,8 +99,10 @@ def func_16(rang: int):
         sum_of_range += i
     return sum_of_range
 
+
 def func_17(num):
     return math.factorial(num)
+
 
 def func_18(s: str):
     vowels = 'aeiou'
@@ -117,6 +121,7 @@ def func_18(s: str):
 def func_19(s: str):
     return ''.join(sorted(list(s)))
 
+
 def func_20(num, num1):
     if num < num1:
         return True
@@ -124,5 +129,3 @@ def func_20(num, num1):
         return False
     else:
         return '1'
-
-print(1 ** (1./3.))
--- tests_practice/test.py
+++ tests_practice/test.py
@@ -4,10 +4,11 @@ from my_func import func_1, func_2, func_3, func_4, func_5, func_6, func_7, \
     func_8, func_9, func_10, func_11, func_12, func_13, func_14, func_15, \
     func_16, func_17, func_18, func_19, func_20
 
+
 class TestTwoList(unittest.TestCase):
 
     def test_func_1(self):
-        a =[1,1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+        a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
         b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
         self.assertEqual(func_1(a, b), [1, 2, 3, 5, 8, 13])
 
@@ -17,45 +18,51 @@ class TestTwoList(unittest.TestCase):
         self.assertEqual(func_2(string), 5)
 
     def test_func_3_true(self):
-        num = 8
         num1 = 27
-        num2 =64
-        self.assertTrue(func_3(num))
+        num2 = 81
+        num3 = 3
+        num4 = 9
         self.assertTrue(func_3(num1))
         self.assertTrue(func_3(num2))
+        self.assertTrue(func_3(num3))
+        self.assertTrue(func_3(num4))
 
     def test_func_3_false(self):
         num = 4
+        num1 = 6
+        num2 = 12
         self.assertFalse(func_3(num))
+        self.assertFalse(func_3(num1))
+        self.assertFalse(func_3(num2))
 
     def test_func_4(self):
         num = 43
         self.assertEqual(func_4(num), 7)
+
     def test_func_5(self):
         a = [0, 2, 3, 4, 6, 7, 10]
         b = [2, 3, 4, 6, 7, 10, 0]
-
         self.assertEqual(func_5(a), b)
 
     def test_func_6(self):
         a = [5, 7, 9, 11]
         b = [5, 7, 9, 10, 11, 16]
-        с = [5, 8, 11, 14]
+        c = [5, 8, 11, 14]
         self.assertTrue(func_6(a))
         self.assertFalse(func_6(b))
-        self.assertTrue(func_6(с))
+        self.assertTrue(func_6(c))
 
     def test_func_7(self):
-        l = [5, 3, 4, 3, 4]
-        self.assertEqual(func_7(l), 5)
+        list_int = [5, 3, 4, 3, 4]
+        self.assertEqual(func_7(list_int), 5)
 
     def test_func_8(self):
-        l = [1,2,3,4,6,7,8]
-        self.assertEqual(func_8(l), 5)
+        list_int = [1, 2, 3, 4, 6, 7, 8]
+        self.assertEqual(func_8(list_int), 5)
 
     def test_func_9(self):
-        l = [1,2,3,(1,2),3]
-        self.assertEqual(func_9(l), 3)
+        list_int_tuple = [1, 2, 3, (1, 2), 3]
+        self.assertEqual(func_9(list_int_tuple), 3)
 
     def test_func_10(self):
         string = 'Hello World and Coders'
