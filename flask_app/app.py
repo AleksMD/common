f55623b5f816850de6b0c3c30094caf69ec584d7
AleksMD <budjonniy291@gmail.com>
2019-10-23 20:40:37 +0300

    Modify style. Add background image

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,8 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def test():
-    return 'Test'
+def home():
+    return render_template('home.html')
+
+@app.route('/fruits')
+def fuits():
+    fruits = ['melon', 'apple', 'strawberry', 'grape']
+    return render_template('fruits.html', fruits=fruits)
+
+@app.route('/vegetables')
+def vegetables():
+    vegetables = ['beans', 'carrot', 'beetroot', 'cucumber']
+    return render_template('vegetables.html', vegetables=vegetables)
 
 
 if __name__ == '__main__':
