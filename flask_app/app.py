89fed4931be6acc3883396c54647acd4e63f7a19
AleksMD <budjonniy291@gmail.com>
2019-10-25 11:29:47 +0300

    Small issues were fixed

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,19 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/fruits')
-def fuits():
-    fruits = ['melon', 'apple', 'strawberry', 'grape']
-    return render_template('fruits.html', fruits=fruits)
+def fruits():
+    fruits_db = ['melon', 'apple', 'strawberry', 'grape']
+    return render_template('fruits.html', fruits=fruits_db)
+
 
 @app.route('/vegetables')
 def vegetables():
-    vegetables = ['beans', 'carrot', 'beetroot', 'cucumber']
-    return render_template('vegetables.html', vegetables=vegetables)
+    vegetables_db = ['beans', 'carrot', 'beetroot', 'cucumber']
+    return render_template('vegetables.html', vegetables=vegetables_db)
 
 
 if __name__ == '__main__':
