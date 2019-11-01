9616381ec4b3c95cfd5dea3772e9a4dba7124b75
AleksMD <budjonniy291@gmail.com>
2019-11-01 11:59:47 +0200

    Create storage for products

--- flask_hw3/blueprints/products/product.py
+++ flask_hw3/blueprints/products/product.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session
 
 
 product_page = Blueprint('product_page', __name__, template_folder='./templates')
 
 
 @product_page.route('/product', methods=['GET', 'POST'])
-@product_page.route('/product/<id>', methods=['GET'])
-def product(id=None):
+@product_page.route('/product/<id_:int>', methods=['GET'])
+def product(id_=None):
+    if 
     return render_template('product.html')
